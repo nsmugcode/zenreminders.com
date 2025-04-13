adfbbcefb44f74873538f75a6c4a0fe290e9c000
Frank <frank@nsmug.com>
2025-04-13 11:12:08 -0700

    Support for showing all Reminders on a single page

---
+++ listing.js
@@ -0,0 +1,25 @@
+reloadAllReminders();
+function reloadAllReminders() {
+	fetch('./reminders.json')
+	.then(response => response.json())
+	.then(data => renderAllReminders(data))
+	.catch(error => console.log(error))
+}
+function renderAllReminders(remindersArray) {
+	var allRemindersDOM = ""
+	for (reminder of remindersArray) {
+		console.log(reminder)
+		let reminderURL = reminder["Source URL"]
+		var sourceLinkTitle = reminder["Source Title"]
+
+		var reminderCardDOM = '<div class="zen-content main-content">'
+		reminderCardDOM += '<h2 id="reminder-text">'+reminder["Content"]+'</h2>';
+		
+		if (reminderURL != null && reminderURL.length > 0){
+			sourceLinkTitle = "<a href='"+reminderURL+"'>"+sourceLinkTitle+"</a>"
+		}
+		reminderCardDOM += '<p id="reminder-source">'+reminder["Source Author"]+"<br>"+sourceLinkTitle+'</div>';
+		allRemindersDOM += reminderCardDOM;
+	}
+	document.getElementById('zen-listing').innerHTML = allRemindersDOM
+}
